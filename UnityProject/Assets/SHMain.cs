8787a779ba96df71c9145504cbb637f6d5f39ede
이상호 <shmhlove@naver.com>
2017-08-26 23:07:57 +0900

    유니티 프로젝트 추가

---
+++ UnityProject/Assets/SHMain.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//using for DllImport
+using System.Runtime.InteropServices;
+
+public class SHMain : MonoBehaviour
+{
+#if !UNITY_EDITOR && UNITY_IOS
+    [DllImport("__Internal")]
+    private static extern void iOSPluginHelloWorld(string strMessage);
+#endif
+
+    void Start ()
+    {
+        UnityEngine.Debug.Log("Unity Start Function in MainScript");
+
+#if !UNITY_EDITOR && UNITY_IOS
+        iOSPluginHelloWorld("Hello~!! iOSPlugin");
+#endif
+    }
+}
